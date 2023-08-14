f19f928a4f0a38110a9548ebc1e7d29ba1494485
zhou-tao <zhoutao_233@163.com>
2023-08-14 23:15:44 +0800

    feat: update footer message

--- src/layouts/admin/footer/index.vue
+++ src/layouts/admin/footer/index.vue
@@ -1,7 +1,7 @@
 <template>
   <el-footer v-if="$store.getters.showFooter" h="footer!">
     <div h="full" flex="center" text="placeholder">
-      MIT Licensed <div i-app-heart text="rose-600!" /> Copyright © 2023-present vue2-admin
+      MIT Licensed <div i-app-heart text="rose-600!" /> Copyright &copy; 2023-present toryz
     </div>
   </el-footer>
 </template>
